1d77f458f7eba938a7ce8f798c0a20e0dc8e0cb0
erjosito <erjosito@hotmail.com>
2017-10-24 21:40:38 +0200

    Minor details

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-az group deployment create -g iaclab --name iacLabDeployment --template-uri https://raw.githubusercontent.com/erjosito/Iac-Test/master/IaCLab\_master.json --parameters @./IaCLab-parameters.json
+az group deployment create --name iacLabDeployment --template-uri https://raw.githubusercontent.com/erjosito/Iac-Test/master/IaCLab\_master.json --parameters @./IaCLab-parameters.json -g iaclab
 duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
